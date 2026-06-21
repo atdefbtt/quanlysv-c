17b68468317ec1b2eea241eaf5193f0769b558db
Nong Thanh Tung <at.defbythanhtung@gmail.com>
2026-06-21 23:46:50 +0700

    fix loi in

--- main.c
+++ main.c
@@ -9,14 +9,15 @@ typedef struct {
 } sinhVien;
 
 int main(){
-    int n, m, i;
-    sinhVien *sv, *sv_c;
+    int n, m, i, t = 0;
+    sinhVien *sv, *sv_c, *sv_m;
     int pass = 0;
     printf("\nNhap so luong sinh vien: ");
     scanf("%d", &n);
     getchar();
 
     sv = (sinhVien*) malloc(n * sizeof(sinhVien));
+    sv_m = (sinhVien*) malloc(n * sizeof(sinhVien));
     sv_c = (sinhVien*) malloc(n * sizeof(sinhVien));
 
     while(1){
@@ -31,33 +32,8 @@ int main(){
         scanf ("%d", &lChon); getchar();
         switch(lChon){
             case 1:
-            for (i = 0 ; i < n; ++i){
-                printf ("\nNhap ma sinh vien: ");
-                scanf("%d", &sv[i].id); getchar ();
-
-                printf("\nNhap ten sinh vien: ");
-                fgets (sv[i].hoTen, sizeof(sv[i].hoTen), stdin);
-                sv[i].hoTen[strcspn(sv[i].hoTen, "\n")] = '\0';
-
-                printf("\nNhap diem: ");
-                scanf ("%f", &sv[i].diem); getchar ();
-
-                printf("\nNhap dia chi: ");
-                fgets (sv[i].diaChi, sizeof(sv[i].diaChi), stdin);
-                sv[i].diaChi[strcspn(sv[i].diaChi, "\n")] = '\0';
-                sv_c[i] = sv[i];
-            }
-            pass = 1;
-            break;
-            case 2:
-            if (pass){  
-                int t = 0;
-                printf ("\nNhap so luong sinh vien muon them: ");
-                scanf ("%d", &t); getchar();
-                m = n + t;
-                sv = (sinhVien*) realloc(sv, m * sizeof(sinhVien));
-                sv_c = (sinhVien*) realloc(sv_c, m * sizeof(sinhVien));
-                for (i = n; i < m; ++i){
+            if (!pass){
+                for (i = 0 ; i < n; ++i){
                     printf ("\nNhap ma sinh vien: ");
                     scanf("%d", &sv[i].id); getchar ();
 
@@ -71,40 +47,98 @@ int main(){
                     printf("\nNhap dia chi: ");
                     fgets (sv[i].diaChi, sizeof(sv[i].diaChi), stdin);
                     sv[i].diaChi[strcspn(sv[i].diaChi, "\n")] = '\0';
+
                     sv_c[i] = sv[i];
-                    break;
                 }
+                pass = 1;
+                break;
             } else {
-                printf ("\nBan chua nhap thong tin sinh vien ban dau");
+                printf ("\nBan da nhap thong tin ban dau roi chi can them\n");
+                break;
+            }
+            break;
+            case 2:
+            if (pass){  
+                free (sv);
+                printf ("\nNhap so luong sinh vien muon them: ");
+                scanf ("%d", &t); getchar();
+               
+                sv_c = (sinhVien*) realloc(sv_c, (n + t) * sizeof(sinhVien));
+                sv_m = (sinhVien*) realloc(sv_m, (n + t) * sizeof(sinhVien));
+
+                for (i = n; i < (n + t); ++i){
+                    printf ("\nNhap ma sinh vien: ");
+                    scanf("%d", &sv_m[i].id); getchar ();
+
+                    printf("\nNhap ten sinh vien: ");
+                    fgets (sv_m[i].hoTen, sizeof(sv_m[i].hoTen), stdin);
+                    sv_m[i].hoTen[strcspn(sv_m[i].hoTen, "\n")] = '\0';
+
+                    printf("\nNhap diem: ");
+                    scanf ("%f", &sv_m[i].diem); getchar ();
+
+                    printf("\nNhap dia chi: ");
+                    fgets (sv_m[i].diaChi, sizeof(sv_m[i].diaChi), stdin);
+                    sv_m[i].diaChi[strcspn(sv_m[i].diaChi, "\n")] = '\0';
+                }
+                n += t;                 
+                pass = 2;
+                break;
+            } else {
+                printf ("\nBan chua nhap thong tin sinh vien ban dau\n");
                 break;
             }
             case 3:
             if (pass){
                 int c;
-                printf ("\n1. In danh sach da nhap");
-                printf ("\n2. In danh sach sau khi them");
-                scanf ("\n%d", &c);
-                switch(c){
-                    case 1:
-                    printf ("\n------------Thong tin sinh vien------------\n");
-                    printf ("\n%-12s %-25s %-5s %-40s\n", "MA SINH VIEN", "HO TEN", "DIEM", "DIA CHI");
-                    for (int i = 0; i < m; ++i){
-                        printf ("%-12d %-25s %-5f %-40s", sv_c[i].id, sv_c[i].hoTen, sv_c[i].diem, sv_c[i].diaChi);
+
+                do{
+                    
+                    printf ("\n1. In danh sach ban dau/truoc do");
+                    printf ("\n2. In danh sach sau khi them");
+                    printf ("\n0. Quay lai menu chuc nang\n");
+                    scanf ("%d", &c);
+                
+                    switch(c){
+                        case 1:
+                        if (pass == 2){
+                            for (i = n; i < (n + t); ++i){
+                                sv_c[i] = sv_m[i];
+                            }
+                        }
+                        printf ("\n------------Thong tin sinh vien------------\n");
+                        printf ("\n%-7s %-25s %-5s %-20s\n", "MSV", "HO TEN", "DIEM", "DIA CHI");
+                        for (int i = 0; i < (n + t); ++i){
+                            printf ("%-7d %-25s %-5.2f %-20s\n", sv_c[i].id, sv_c[i].hoTen, sv_c[i].diem, sv_c[i].diaChi);
+                        }
+                        break;
+                        case 2:
+                        if (pass == 2){
+                                for (i = 0; i < n; ++i){
+                                sv_m[i] = sv_c[i];
+                            }                            
+                            printf ("\n------------Thong tin sinh vien------------\n");
+                            printf ("\n%-7s %-25s %-5s %-40s\n", "MSV", "HO TEN", "DIEM", "DIA CHI");
+                            for (int i = 0; i < (n + t); ++i){
+                                printf ("%-7d %-25s %-5.2f %-40s\n", sv_m[i].id, sv_m[i].hoTen, sv_m[i].diem, sv_m[i].diaChi);
+                            }
+                            break;
+                        } else {
+                            printf ("\nChuc nang bi khoa do ban chua them sinh vien nao moi\n");
+                        }
                     }
-                    case 2:
-                    printf ("\n------------Thong tin sinh vien------------\n");
-                    printf ("\n%-12s %-25s %-5s %-40s\n", "MA SINH VIEN", "HO TEN", "DIEM", "DIA CHI");
-                    for (int i = 0; i < m; ++i){
-                        printf ("%-12d %-25s %-5f %-40s", sv[i].id, sv[i].hoTen, sv[i].diem, sv[i].diaChi);
-                    }
+
+                } while(c != 0);
+                if (c == 0) {
+                    break;
                 }
-                break;
+
             } else {
-                printf ("Chua co du lieu sinh vien");
+                printf ("\nChua co du lieu sinh vien\n");
                 break;
             }
             case 0:
-            free (sv);
+            free (sv_m);
             free (sv_c);
             return 0;
 
@@ -114,3 +148,5 @@ int main(){
 
     return 0;
 }
+
+/// loi in
