ade6e30d9cf6ac9a9f1683e1baaca0ae9d24b55b
Nong Thanh Tung <at.defbythanhtung@gmail.com>
2026-06-22 16:18:37 +0700

    fix loi & them chuc nang

--- main.c
+++ main.c
@@ -10,15 +10,13 @@ typedef struct {
 
 int main(){
     int n, m, i, t = 0;
-    sinhVien *sv, *sv_c, *sv_m;
-    int pass = 0;
+    sinhVien *sv, *sv_c, *sv_t;
+    int pass = 0, check = 0;
     printf("\nNhap so luong sinh vien: ");
     scanf("%d", &n);
     getchar();
 
     sv = (sinhVien*) malloc(n * sizeof(sinhVien));
-    sv_m = (sinhVien*) malloc(n * sizeof(sinhVien));
-    sv_c = (sinhVien*) malloc(n * sizeof(sinhVien));
 
     while(1){
         int lChon;
@@ -48,7 +46,6 @@ int main(){
                     fgets (sv[i].diaChi, sizeof(sv[i].diaChi), stdin);
                     sv[i].diaChi[strcspn(sv[i].diaChi, "\n")] = '\0';
 
-                    sv_c[i] = sv[i];
                 }
                 pass = 1;
                 break;
@@ -59,27 +56,26 @@ int main(){
             break;
             case 2:
             if (pass){  
-                free (sv);
+                free (sv_t);
                 printf ("\nNhap so luong sinh vien muon them: ");
                 scanf ("%d", &t); getchar();
                
-                sv_c = (sinhVien*) realloc(sv_c, (n + t) * sizeof(sinhVien));
-                sv_m = (sinhVien*) realloc(sv_m, (n + t) * sizeof(sinhVien));
+                sv = (sinhVien*) realloc(sv, (n + t) * sizeof(sinhVien));
 
                 for (i = n; i < (n + t); ++i){
                     printf ("\nNhap ma sinh vien: ");
-                    scanf("%d", &sv_m[i].id); getchar ();
+                    scanf("%d", &sv[i].id); getchar ();
 
                     printf("\nNhap ten sinh vien: ");
-                    fgets (sv_m[i].hoTen, sizeof(sv_m[i].hoTen), stdin);
-                    sv_m[i].hoTen[strcspn(sv_m[i].hoTen, "\n")] = '\0';
+                    fgets (sv[i].hoTen, sizeof(sv[i].hoTen), stdin);
+                    sv[i].hoTen[strcspn(sv[i].hoTen, "\n")] = '\0';
 
                     printf("\nNhap diem: ");
-                    scanf ("%f", &sv_m[i].diem); getchar ();
+                    scanf ("%f", &sv[i].diem); getchar ();
 
                     printf("\nNhap dia chi: ");
-                    fgets (sv_m[i].diaChi, sizeof(sv_m[i].diaChi), stdin);
-                    sv_m[i].diaChi[strcspn(sv_m[i].diaChi, "\n")] = '\0';
+                    fgets (sv[i].diaChi, sizeof(sv[i].diaChi), stdin);
+                    sv[i].diaChi[strcspn(sv[i].diaChi, "\n")] = '\0';
                 }
                 n += t;                 
                 pass = 2;
@@ -91,40 +87,50 @@ int main(){
             case 3:
             if (pass){
                 int c;
-
+                sv_t = (sinhVien*) malloc(t * sizeof(sinhVien));
+                for (i = 0; i < t; ++i){
+                    sv_t[i] = sv[i + n - t];
+                }
                 do{
                     
                     printf ("\n1. In danh sach ban dau/truoc do");
                     printf ("\n2. In danh sach sau khi them");
+                    printf ("\n3. In danh sach sinhvien vua them");
                     printf ("\n0. Quay lai menu chuc nang\n");
                     scanf ("%d", &c);
                 
                     switch(c){
                         case 1:
-                        if (pass == 2){
-                            for (i = n; i < (n + t); ++i){
-                                sv_c[i] = sv_m[i];
-                            }
-                        }
+
                         printf ("\n------------Thong tin sinh vien------------\n");
                         printf ("\n%-7s %-25s %-5s %-20s\n", "MSV", "HO TEN", "DIEM", "DIA CHI");
-                        for (int i = 0; i < (n + t); ++i){
-                            printf ("%-7d %-25s %-5.2f %-20s\n", sv_c[i].id, sv_c[i].hoTen, sv_c[i].diem, sv_c[i].diaChi);
+                        for (int i = 0; i < n - t ; ++i){
+                            printf ("%-7d %-25s %-5.2f %-20s\n", sv[i].id, sv[i].hoTen, sv[i].diem, sv[i].diaChi);
                         }
                         break;
                         case 2:
-                        if (pass == 2){
-                                for (i = 0; i < n; ++i){
-                                sv_m[i] = sv_c[i];
-                            }                            
+                        if (pass == 2){                           
                             printf ("\n------------Thong tin sinh vien------------\n");
                             printf ("\n%-7s %-25s %-5s %-40s\n", "MSV", "HO TEN", "DIEM", "DIA CHI");
-                            for (int i = 0; i < (n + t); ++i){
-                                printf ("%-7d %-25s %-5.2f %-40s\n", sv_m[i].id, sv_m[i].hoTen, sv_m[i].diem, sv_m[i].diaChi);
+                            for (int i = 0; i < n; ++i){
+                                printf ("%-7d %-25s %-5.2f %-40s\n", sv[i].id, sv[i].hoTen, sv[i].diem, sv[i].diaChi);
                             }
                             break;
                         } else {
                             printf ("\nChuc nang bi khoa do ban chua them sinh vien nao moi\n");
+                            break;
+                        }
+                        case 3:
+                        if (pass == 2){                           
+                            printf ("\n------------Thong tin sinh vien------------\n");
+                            printf ("\n%-7s %-25s %-5s %-40s\n", "MSV", "HO TEN", "DIEM", "DIA CHI");
+                            for (int i = 0; i < t; ++i){
+                                printf ("%-7d %-25s %-5.2f %-40s\n", sv_t[i].id, sv_t[i].hoTen, sv_t[i].diem, sv_t[i].diaChi);
+                            }
+                            break;
+                        } else {
+                            printf ("\nChuc nang bi khoa do ban chua them sinh vien nao moi\n");
+                            break;
                         }
                     }
 
@@ -138,8 +144,8 @@ int main(){
                 break;
             }
             case 0:
-            free (sv_m);
-            free (sv_c);
+            free (sv_t);
+            free (sv);
             return 0;
 
         }
@@ -149,4 +155,4 @@ int main(){
     return 0;
 }
 
-/// loi in
+/// loi logic 2 bien sv_c & sv_m
